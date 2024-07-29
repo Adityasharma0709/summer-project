bdc4ca1685a5c6ec75d63b09d0dc9e81898277e9
Adityasharma0709 <7rockyrocky@gmail.com>
2024-07-29 21:57:26 +0530

    change 2

---
+++ src/components/navigationBar.js
@@ -0,0 +1,11 @@
+import "../css/button.css";
+function navigationBar() {
+  return (
+    <nav className="navigationBar">
+      <h1 className="heading">B.Planet</h1>
+      <button className="contact">contact Us</button>
+    </nav>
+  );
+}
+
+export default navigationBar;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,22 @@
-import Heading from "../components/heading";
-import Contact from "../components/contact";
+import Logo1 from "../assests/Hero image 1.jpg";
+import Logo2 from "../assests/hero image 2.png";
 import "../css/universal.css";
+import "../css/button.css"
+import "../css/homePage.css"
+import Navigation from "../components/navigationBar";
 function Home() {
   return (
-    <nav style={{ display:"flex" ,justifyContent:"space-between",alignItems:"center"}}>
-      <Heading />
-      <Contact />
-    </nav>
+    <div className="top">
+      <Navigation />
+      <div className="content">
+        <div className="heading2"><p>‘As interesting as a plant’</p></div>
+        <div className="logo">
+        <img src={Logo2} alt="hero2"></img>
+        <img src={Logo1} alt="hero1"></img>
+        </div>
+
+      </div>
+    </div>
   );
 }
 export default Home;
